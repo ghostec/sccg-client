1bcf6b0f31eafbd39e84ee01c91a1a918509e8a6
Yuri Brito <yuriabrito@gmail.com>
2018-02-24 13:55:37 -0300

    boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: [
+    '@babel/polyfill',
+    'react-hot-loader/patch',
+    './src/index.jsx'
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    modules: ['node_modules', path.resolve('./src')]
+  },
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve('./assets/index.html'),
+      filename: 'index.html'
+    })
+  ],
+  devServer: {
+    host: '0.0.0.0',
+    hot: true
+  }
+}
